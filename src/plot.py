9c51f9167cedd17ebedab4d0d418fc65a02d4378
Jerin George Mathew <jerin.edassery95@gmail.com>
2020-11-15 00:36:14 +0100

    plot using matplotlib

---
+++ src/plot.py
@@ -0,0 +1,41 @@
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+
+def compare_boxplots(data_a, data_b, label_a, label_b, use_plotly, figsize=(16,8)):
+    if use_plotly:
+        fig = go.Figure()
+        fig.add_trace(go.Box(y=data_a, name=label_a))
+        fig.add_trace(go.Box(y=data_b, name=label_b))
+        return fig.show()
+    else:
+        fig, ax = plt.subplots(figsize=(16,8))
+        ax.boxplot([data_a, data_b], labels=[label_a, label_b])
+        plt.show()
+        
+        
+def compare_lineplots(data_x, data_ya, data_yb, label_a, label_b, x_label, y_label, use_plotly, figsize=(16,8)):
+    if use_plotly:
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data_x,
+                                 y=data_ya,
+                                 mode='markers+lines',
+                                 name=label_a))
+        fig.add_trace(go.Scatter(x=data_x,
+                                 y=data_yb,
+                                 mode='markers+lines',
+                                 name=label_b))
+        fig.update_layout(xaxis_title=x_label,yaxis_title=y_label)
+        fig.show()
+    
+    else:
+        fig, ax = plt.subplots(figsize=(12,8))
+        ax.plot(data_x, data_ya, label=label_a)
+        ax.plot(data_x, data_yb, label=label_b)
+        plt.legend(loc="lower right")
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        fig.show()
